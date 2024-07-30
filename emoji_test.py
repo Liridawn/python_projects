eea9c8d3d83cfd345d7c733373ae8d60fcce61c5
Liridawn <liridon.bislimi@hotmail.com>
2024-07-30 21:33:12 +0200

    New update

--- emoji_test.py
+++
@@ -1,5 +0,0 @@
-import emoji
-
-print(emoji.emojize("Välkommen! :thumbs_up: :red_heart: :Denmark:", language='alias'))
-print(emoji.emojize("Välkommen! :thumbs_up: :red_heart: :denmark_flag:", language='alias'))
-print(emoji.emojize("Välkommen! :thumbs_up: :red_heart: :flag_for_Denmark:"))
